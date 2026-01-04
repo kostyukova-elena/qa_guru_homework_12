a4aa1ef0a98128cae40b2c364fcecb8357f84518
ekostyukova <kostyukova_ea@mail.ru>
2026-01-04 15:02:01 +0500

    fix1

--- src/test/java/pages/TestBase.java
+++ src/test/java/pages/TestBase.java
@@ -17,6 +17,7 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 4000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.of(
                 "enableVNC", true,
